949aba042c1848597b649de82efec27db356d864
m.podosenovmiac <podosenovmiac@yandex.com>
2023-03-09 12:55:22 +0300

    sorter-del

--- Validator.js
+++ Validator.js
@@ -7,10 +7,19 @@ class Validator {
     this.regexpEnd = new RegExp('[^\-0-9_]{1}$', 'ig');
     this.regexpNumbers = new RegExp('[0-9]{4}', 'ig');
     this.regexpMain = new RegExp('[a-z\-_]+', 'ig');
+    this.regexpMainInvalid = new RegExp('[^a-z\-_0-9]+', 'ig');
   }
 
   validateUsername() {
-    return this.username.match(this.regexpStart) && this.username.match(this.regexpEnd) && !this.username.match(this.regexpNumbers) && this.username.match(this.regexpMain);
+    let isLatin = true;
+
+    if (this.username.match(this.regexpMain)) {
+      if (this.username.match(this.regexpMain).length > 1 && this.username.match(this.regexpMainInvalid) ) {
+        isLatin = false;
+      }
+    }
+    
+    return this.username.match(this.regexpStart) && this.username.match(this.regexpEnd) && !this.username.match(this.regexpNumbers) && this.username.match(this.regexpMain) && isLatin;
   }
 }
 
